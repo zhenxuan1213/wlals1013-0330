159170e100556f5a549482f28a9919911923f5b4
zhenxuan1213 <1013wlals1995@gmail.com>
2021-03-30 17:16:11 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<h1>歡迎光臨葉貞萱網站</h1>
+<button><a href='/wlals1013/'>HOME</a></button>
+<button><a href='http://dba.nkust.edu.tw/'>企業管理系</a></button>
+<button><a href='http://tzukuan.nkust.org'>計畫網站</a></button>
+<select onchange='location.href="/wlals1013/"+this.value'>
+	<option value="0323-1.php">請選擇要執行的練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0330-1.php">0330-1</option>	
+</select>
+<hr>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>葉貞萱的練習網頁</title>
+	<style>
+		body {
+			  font-family: 標楷體;
+			  background-color: #C2C2FF;
+		}
+		h1 {
+              color: white;
+              text-align: center;
+}
+	</style>
+</head>
+<body>
+<h1>歡迎光臨葉貞萱網站</h1>
+<button><a href='/wlals1013/'>HOME</a></button>
+<button><a href='http://dba.nkust.edu.tw/'>企業管理系</a></button>
+<button><a href='http://tzukuan.nkust.org'>計畫網站</a></button>
+<select onchange='location.href="/wlals1013/"+this.value'>
+	<option value="0323-1.php">請選擇要執行的練習</option>
+	<option value="0323-1.php">0323-1</option>
+	<option value="0330-1.php">0330-1</option>	
+</select>
+<hr>
+
+
+
+<p>有什麼問題歡迎連絡 1013wlals1995@gmail.com</p>
+</body>
+</html>
